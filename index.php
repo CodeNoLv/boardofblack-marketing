1db1825d76d0e6883d703519799ce51cfae4b851
Ivars Bariss <bariss.ivars@gmail.com>
2017-06-18 01:10:26 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Board of Black | Comming soon</title>
+
+    <style type="text/css">
+        .centerImage
+        {
+            text-align:center;
+            display:block;
+        }
+    </style>
+</head>
+<body>
+
+<p style="text-align:center;"><img src="logo.svg"></p>
+<h1 style="color: white;">Organize public and in-house functional fitness tournaments and compete in global and gym's ranking, </h1>
+
+</body>
+</html>
